866837d58be9f08805daa4ea1ffd7a538b79956c
nahidhsynli <95674042+nahidhsynli@users.noreply.github.com>
2022-04-20 16:52:25 +0400

    Code:
    CREATE TABLE istifadeciler(
    ist_id int,
    ist_ad varchar2(20),
    ist_soyad varchar2(20),
    ist_login varchar2(20),
    ist_password varchar2(20)
    );
    
    Insert INTO istifadeciler(ist_id,ist_ad,ist_soyad,ist_login,ist_password)
    VALUES(1,'Nahid','Huseynli','nahid123','nahid2003');
    
    Insert INTO istifadeciler(ist_id,ist_ad,ist_soyad,ist_login,ist_password)
    VALUES(2,'Anar','Babazade','anar123','anar2003');
    
    Insert INTO istifadeciler(ist_id,ist_ad,ist_soyad,ist_login,ist_password)
    VALUES(3,'Nihad','Soltanov','nihad123','nihad2003');
    
    Insert INTO istifadeciler(ist_id,ist_ad,ist_soyad,ist_login,ist_password)
    VALUES(4,'Fuad','Nagiyev','fuad123','fuad2003');
    
    Insert INTO istifadeciler(ist_id,ist_ad,ist_soyad,ist_login,ist_password)
    VALUES(5,'Kenan','Quluzade','kenan123','kenan2003');
    
    Insert INTO istifadeciler(ist_id,ist_ad,ist_soyad,ist_login,ist_password)
    VALUES(6,'Behmen','Bunyatov','behmen123','behmen2003');
    
    Insert INTO istifadeciler(ist_id,ist_ad,ist_soyad,ist_login,ist_password)
    VALUES(7,'Elsen','Qasimov','elsen123','elsen2003');
    
    Insert INTO istifadeciler(ist_id,ist_ad,ist_soyad,ist_login,ist_password)
    VALUES(8,'Vusal','Eliyev','vusal123','vusal2003');
    
    Insert INTO istifadeciler(ist_id,ist_ad,ist_soyad,ist_login,ist_password)
    VALUES(9,'Resad','Ibadov','resad123','resad2003');
    
    Insert INTO istifadeciler(ist_id,ist_ad,ist_soyad,ist_login,ist_password)
    VALUES(10,'Elnur','Huseynov','elnur123','elnur2003');
    
    SELECT* FROM istifadeciler;

---
+++ istifadeciler.sql
@@ -0,0 +1,19 @@
+--------------------------------------------------------
+--  File created - Wednesday-April-20-2022   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table ISTIFADECILER
+--------------------------------------------------------
+
+  CREATE TABLE "SYSTEM"."ISTIFADECILER" 
+   (	"IST_ID" NUMBER(*,0), 
+	"IST_AD" VARCHAR2(20 BYTE), 
+	"IST_SOYAD" VARCHAR2(20 BYTE), 
+	"IST_LOGIN" VARCHAR2(20 BYTE), 
+	"IST_PASSWORD" VARCHAR2(20 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
